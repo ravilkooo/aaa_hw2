d8dd2d8ebab892aed26ca13cd5271417c89f0a47
Ravil Sultanov <ravilka.zftsh@yandex.ru>
2022-10-19 19:21:32 +0500

    Исправление 2 после проверки

--- cnt_vectorizer.py
+++ cnt_vectorizer.py
@@ -1,5 +1,6 @@
 class CountVectorizer:
     def __init__(self):
+        # Набор фичей корпуса
         self._feature_names = []
 
     def fit_transform(self, corpus: list[str]) -> list[list[int]]:
@@ -10,15 +11,12 @@ class CountVectorizer:
         :param corpus: текстовый корпус
         :return: соответствующая корпусу терм-документационная матрица
         """
+        # В данном методе
+        # сохраняем набор _feature_names как атрибут класса,
+        # т.к. он понадобиться для передачи набора фичей в методе get_feature_names()
 
-        '''
-        # отдельно храним набор _feature_names как атрибут класса, т.к. он понадобиться при выводе набора фичей
-        # в методе get_feature_names()
-        # массив словарей - каждая i-ый словарь содержит слова (ключи) i-ого предложения
-        # и количество повторений данного слова в это предложениее (значение)
-        '''
-        # _feature_names - набор фичей корпуса
-        self._feature_names = set()
+        # feature_names_set - набор фичей корпуса
+        feature_names_set = set()
         # docterm_dict - массив, где i-ый элемент представляет собой словарь, где
         # ключ: фича из i-ого предложения корпуса
         # значение: количество повторений данной фичи в i-ом предложении корпуса
@@ -34,16 +32,15 @@ class CountVectorizer:
             # сохраняем словарь фичей предложения sentence
             docterm_dict.append(feature_dict)
             # пополяняем множетсво фичей новыми фичами из предложения sentence
-            self._feature_names = self._feature_names.union(feature_dict.keys())
-        # преобразуем множество фичей в лист фичей
-        self._feature_names = list(self._feature_names)
+            feature_names_set = feature_names_set.union(feature_dict.keys())
+        # Преобразуем и сохраняем множество фичей в лист фичей как атрибут класса
+        self._feature_names = list(feature_names_set)
         # print(f'feature_names = {self._feature_names}')
         # print(f'docterm_dict = {docterm_dict}')
         # Формируем терм-документную матрицу:
         # считаем сколько раз в каждом предложении встречается каждая фича из _feature_names
-        # с помощью сохранённых словарей со счётчиками фичей внутри одного предложенияЦ
+        # с помощью сохранённых словарей со счётчиками фичей внутри одного предложения
         return [ [feature_dict.get(feature) or 0 for feature in self._feature_names] for feature_dict in docterm_dict ]
-
 
     def get_feature_names(self) -> list[str]:
         """
@@ -61,10 +58,12 @@ if __name__ == '__main__':
         'Pasta Pomodoro Fresh ingredients Parmesan to taste'
     ]
 
-
+    # Проверяем работу метода
+    # Создаём объект
     vectorizer = CountVectorizer()
+    # Получаем терм-док матрицу
     count_matrix = vectorizer.fit_transform(corpus)
-
+    # Выводим все фичи с помощью метода get_feature_names()
     print(vectorizer.get_feature_names())
-
+    # Выводим терм-док матрицу
     print(count_matrix)
